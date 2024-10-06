9f23ac9d24f43583a6e7ac045d75e6bc2525dd5e
whale <3052748978@qq.com>
2024-10-06 15:46:23 +0800

    errors6

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -14,30 +14,30 @@
 use std::num::ParseIntError;
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
+// 自定义的错误类型
 #[derive(PartialEq, Debug)]
 enum ParsePosNonzeroError {
     Creation(CreationError),
     ParseInt(ParseIntError),
 }
 
-/* 把自定义的error放在这里 */
+// 错误类型转换实现
 impl ParsePosNonzeroError {
+    // 处理 CreationError 类型的错误
     fn from_creation(err: CreationError) -> ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
-    // TODO: add another error conversion function here.
-    fn from_parseint(err: ParseInt) -> ParsePosNonzeroError{
-      ParsePosNonzeroError::ParseInt(err)
+
+    // 处理 ParseIntError 类型的错误
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
     }
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
-    // TODO: change this to return an appropriate error instead of panicking
-    // when `parse()` returns an error.
-    /* parse error */
-    let x: i64 = s.parse().unwrap().map_err(ParseIntError::from_creation)?;
-    PositiveNonzeroInteger::new(x).map_err(ParseIntError::from_parseint);
-
+    // 错误处理，返回 Result 而不是使用 unwrap
+    let x: i64 = s.parse::<i64>().map_err(ParsePosNonzeroError::from_parseint)?;  // 处理 parse 错误
+    PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)   // 处理创建正数错误
 }
 
 // Don't change anything below this line.
