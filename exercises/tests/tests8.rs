1e3de6575d534f85b6a3d025cd95b4d2740905b2
whale <3052748978@qq.com>
2024-10-07 09:56:08 +0800

    exercise test

--- exercises/tests/tests8.rs
+++ exercises/tests/tests8.rs
@@ -7,7 +7,7 @@
 // Execute `rustlings hint tests8` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {}
 
@@ -17,7 +17,7 @@ mod tests {
 
     #[test]
     fn test_success() {
-        #[cfg(feature = "pass")]
+        #[cfg(pass)]
         return;
 
         panic!("no cfg set");
